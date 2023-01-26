5772b72fb20c8065bbfd549236e07780bdd04805
Oleg Deryabin <olegderyabin22@gmail.com>
2023-01-26 21:11:50 +0600

    26.01.2022 2111

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,3 +9,31 @@ burgerBtn.addEventListener("click", () => {
   burgerHeadings.classList.toggle("active");
   body.classList.toggle("lock");
 });
+
+$(document).ready(function () {
+  // ! Menu
+  var menuParentLiIcon =
+      '<svg style="width:7px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-down" class="svg-inline--fa fa-angle-down fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#969ba2" d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"></path></svg>',
+    menuParentLi = $(".item-parent_js"),
+    submenuSwitcherClass = "toggle_submenu_js";
+
+  if ($(window).width() > 992) {
+    menuParentLi
+      .children("a")
+      .append('<span class="arrow">' + menuParentLiIcon + "</span>");
+  } else {
+    menuParentLi.append(
+      '<span class="arrow ' +
+        submenuSwitcherClass +
+        '">' +
+        menuParentLiIcon +
+        "</span>"
+    );
+
+    $("." + submenuSwitcherClass).click(function () {
+      let parent = $(this).parent();
+      parent.children(".menu").slideToggle().toggleClass("active");
+    });
+  }
+  // ! Menu end
+});
